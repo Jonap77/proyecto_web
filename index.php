575c6b8d963fae7966308e3d88f7159a80d659a4
jonathan <ulloacarrascojonathaneduardo@gmail.com>
2025-10-20 22:14:25 -0600

    Renombrar index.html a index.php y habilitar PHP

---
+++ index.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preload" href="style_inicio.css" as="style"> <!--cargar mas rapido la pagina-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+
+    <link rel="preload" href="style_inicio.css" as="style"> <!--cargar mas rapido la pagina-->
+    <link rel="stylesheet" href="style_inicio.css">
+        <link rel="stylesheet" href="normalize.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    <title>Citas</title>
+</head>
+<body>
+
+
+                <header>
+                        <h1 class="animate__animated animate__jackInTheBox titulo">
+                                    Bienvenido a HY BarberShop</h1>
+                    </header>
+
+<!-- Codigo de menu -->
+      
+     <main class="principal">
+
+                <div class="animate__animated animate__bounce servicios">
+                    <a href="#" class="productos">
+                        <section class="servicio">
+                            <div class="iconos">
+                            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-week"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" /><path d="M16 3v4" /><path d="M8 3v4" /><path d="M4 11h16" /><path d="M7 14h.013" /><path d="M10.01 14h.005" /><path d="M13.01 14h.005" /><path d="M16.015 14h.005" /><path d="M13.015 17h.005" /><path d="M7.01 17h.005" /><path d="M10.01 17h.005" /></svg>
+                            </div>
+                                    <h3>Gestiona tus citas, clientes distinguidos y más.</h3>
+                                </section>
+                    </a>
+             
+                       
+     <a class ="productos" href="#">
+            <section class="servicio">
+                    <div class="iconos">
+                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-building-store"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l18 0" /><path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" /><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" /></svg>
+                    </div>
+                <h3>Agrega, edita, elimina o busca los productos en tu inventario</h3>
+            </section>
+        </a>
+                            
+
+                                   <a href="#" class="productos">
+                                   <section class= "servicio">
+                                    <div class="iconos">
+                                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-cut"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M9.15 14.85l8.85 -10.85" /><path d="M6 4l8.85 10.85" /></svg>
+                                    </div>
+
+                                        <h3>¡Da una mejor presentación de tus servicios!</h3>
+                                    </section>
+                                   </a>
+                                   
+                     
+                        </div>
+
+         </main> <!--Contenido principal-->
+
+
+
+  
+                        <div class="contenedor-footer">
+
+<footer class="animate__animated animate__bounceInLeft footer">
+
+<div class="contenedor-iconos">
+
+    <div class="icono">
+        <a href="https://wa.me/529371214462">
+        <svg class="what" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2"> <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path> <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path> </svg> 
+        </a>
+        </div>
+    
+   
+        
+<div class="icono">
+    <a href="https://www.facebook.com/share/1HUV1ED6Mw/?mibextid=wwXIfr">
+    <svg class="face" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2"> <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path> </svg> 
+    </a>
+</div>
+   
+</div>
+
+
+</footer>
+
+</div>
+
+</body>
+
+</html>
+
